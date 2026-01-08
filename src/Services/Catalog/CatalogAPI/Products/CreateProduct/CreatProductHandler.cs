6267eb5ff7294dc67db20b4467d895bf2b6a9ae8
PremalWonderbiz <premal.kadam@wonderbiz.in>
2026-01-08 18:14:24 +0530

    setup root project structure created building blocks class library for common logics and packages and created calatalog service microservice with create product post endpoint implementation

---
+++ src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreatProductHandler.cs
@@ -0,0 +1,30 @@
+﻿namespace CatalogAPI.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<string> category, string Description, string ImageFile, decimal Price)
+        : ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+
+    internal class CreatProductCommandHandler 
+        : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            //create a product entity from command object
+            //save to database
+            //return CreateProductResult
+
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price
+            };
+
+
+
+           return new CreateProductResult(Guid.NewGuid());
+        }
+    }
+}
